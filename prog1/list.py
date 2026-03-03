669e5152c5535b81c2e455aaa42e2f6c996ffe96
Likhithns11 <likhthsnadiger17@gmail.com>
2026-03-03 23:20:34 +0530

    add

--- prog1/list.py
+++ prog1/list.py
@@ -11,3 +11,7 @@ class1.remove("sam")
 print(class1)
 print(class1[1])
 print(len(class1))
+numbers = [3,67,5,4,646454,64,64,54123,3161,776,6,55,5,4,54,556,67,5,5,4]
+print(numbers)
+numbers.sort()
+print(numbers)
