42a9ac5749328921322b5925c99e80028e8edd64
Likhithns11 <likhthsnadiger17@gmail.com>
2026-03-03 21:54:58 +0530

    add

--- prog1/list.py
+++ prog1/list.py
@@ -2,5 +2,12 @@ import os
 class1 = ["ramesh","suresh","ajay", "ram"]
 class2 =("likhith","ravi")
 
+
 print(class1)
 print(class2)
+class1.append("sam")
+print(class1)
+class1.remove("sam")
+print(class1)
+print(class1[1])
+print(len(class1))
